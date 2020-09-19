6ceaf672869306a0f4eff4f87aabdba7374b86c2
wahsandaruwan <wahsandaruwan6@gmail.com>
2020-09-19 23:28:47 +0530

    Added Scroll Effects

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    // For Sticky Navigation
+    // Sticky Navigation
     $('.js--section-features').waypoint(function(direction){
         if(direction == "down"){
             $('nav').addClass('sticky');
@@ -12,7 +12,7 @@ $(document).ready(function(){
         offset: '60px;'
     });
 
-    // Scroll Buttons
+    // Call to Action Buttons Smooth Scrolling
     $('.js--scroll-to-plans').click(function(){
         $('html, body').animate({
             scrollTop: $('.js--section-plans').offset().top
@@ -23,5 +23,30 @@ $(document).ready(function(){
         $('html, body').animate({
             scrollTop: $('.js--section-features').offset().top
         }, 1000);
+    });
+
+    // Navigation Smooth Scrolling
+    $('.scroll').click(function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    });
+
+    // Set active class for the current section
+    $(window).scroll(function(){
+        // Get scrollbar position from the top
+        var scrollbarLoc = $(this).scrollTop();
+        $('.scroll').each(function(){
+            // Get actual offset from the top
+            var sectionOffset = $(this.hash).offset().top - 50;
+            if(sectionOffset <= scrollbarLoc){
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+            else if(scrollbarLoc <= 100){
+                $(this).parent().siblings().removeClass('active');
+            }
+        })
     });
 });
